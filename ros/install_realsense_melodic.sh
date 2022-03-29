a0b5e53d45a5301a33181a568150531340369c04
nano <jiangxt21@lzu.edu.cn>
2022-03-29 20:24:16 +0800

    init nano

--- ros/install_realsense_melodic.sh
+++ ros/install_realsense_melodic.sh
@@ -9,12 +9,14 @@ echo -e "\e[0;31mDO NOT\e[0m complie librealsense 2 with default config."
 echo -e "\e[0;34mInstall dependences\e[0m"
 
 
-sudo apt-get install python-defusedxml python-netifaces gnupg
+sudo apt-get install -y python-defusedxml python-netifaces gnupg
+
 
 echo -e "\e[0;34mDownload and install\e[0m"
 
-/home/pi/catkin_ws/src
-git clone https://github.com/IntelRealSense/realsense-ros.git
+cd /home/pi/catkin_ws/src
+proxychains4 git clone https://github.com/pal-robotics/ddynamic_reconfigure.git
+proxychains4 git clone https://github.com/IntelRealSense/realsense-ros.git
 cd /home/pi/catkin_ws/src/realsense-ros/
 
 # Do not checkout in latest version like https://blog.csdn.net/AnChenliang_1002/article/details/109454465
@@ -22,8 +24,19 @@ cd /home/pi/catkin_ws/src/realsense-ros/
 proxychains4 git checkout `git tag | sort -V | grep -P "^2.\d+\.\d+" | tail -1`
 
 cd /home/pi/catkin_ws/
-sudo -u pi /home/pi/catkin_ws/src/catkin/bin/catkin_make_isolated  --install-space=/opt/ros/melodic --install --pkg realsense2_camera realsense2_description
+sudo -u pi /home/pi/catkin_ws/src/catkin/bin/catkin_make_isolated  --install-space=/opt/ros/melodic --install --pkg ddynamic_reconfigure realsense2_camera realsense2_description
 
 echo -e "\e[0;34mDone !\e[0m"
+
+# initial_reset:=true
+
+# roslaunch realsense2_camera rs_camera.launch filters:=pointcloud depth_width:=424 color_width:=424 depth_height:=240 color_height:=240 depth_fps:=15 color_fps:=15 pointcloud_texture_stream:=RS2_STREAM_COLOR wait_for_device_timeout:=-1.0 initial_reset:=true
+
+# roslaunch realsense2_camera rs_camera.launch filters:=pointcloud depth_width:=848 color_width:=848 depth_height:=480 color_height:=480 depth_fps:=15 color_fps:=15 pointcloud_texture_stream:=RS2_STREAM_COLOR wait_for_device_timeout:=-1.0
+
+echo -e "Try a specific command like \n \e[0;34mroslaunch realsense2_camera rs_camera.launch filters:=pointcloud depth_width:=424 color_width:=424 depth_height:=240 color_height:=240 depth_fps:=15 color_fps:=15 pointcloud_texture_stream:=RS2_STREAM_COLOR !\e[0m"
+
+
+
 
 
